3d26330fe010c8dc667a82706500db6a44fdd120
xiaojingzhao <xiaojingzhao091@gmail.com>
2020-08-18 22:44:30 +0800

    [xiaojingzhao] daily update

--- xiaojingzhao/examples/proxy/hack-prototype.js
+++ xiaojingzhao/examples/proxy/hack-prototype.js
@@ -8,23 +8,20 @@ let handlers = {
   },
 };
 
-let obj1 = new Proxy(
-  {
-    name: "obj-1",
-    foo() {
-      console.log("foo: ", this.name);
-    },
+let originalObj = {
+  name: "obj-1",
+  foo() {
+    console.log("foo: ", this.name);
   },
-  handlers
-);
+};
 
+let obj1 = new Proxy(originalObj, handlers);
 let obj2 = Object.assign(Object.create(obj1), {
   name: "obj-2",
   bar() {
     console.log("bar: ", this.name);
   },
 });
-
-obj1[Symbol.for("[Prototype]]")] = obj2; // 每天绑定在target上，会报错
+obj1[Symbol.for("[[Prototype]]")] = obj2; // 绑定在target上，会报错w
 
 obj1.bar();
